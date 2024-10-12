c997b2df390b9879a88cfbcf6db6116696155bdd
Pedro Souza de Freitas <pedro.souza@econverse.com.br>
2024-10-12 08:50:24 -0300

    Publish 3 from localhost into origin/feature/28718/header

--- components/header/MinicartEmpty.tsx
+++ components/header/MinicartEmpty.tsx
@@ -4,7 +4,27 @@ interface Props {
   content: MinicartEmptyProps | null;
 }
 
-export function MinicartEmpty(props: Props) {
-  console.log({ props });
-  return <h1>Empty</h1>;
+export function MinicartEmpty({ content }: Props) {
+  console.log({ content });
+  return (
+    <div>
+      <div>
+        {content?.icon && <img src={content.icon} alt="Sacola" />}
+        <h4>{content?.title}</h4>    
+      </div>
+
+      <div>
+        <h4>{content?.itemsTitle}</h4>
+
+        {
+          content?.items.map(item => (
+            <a href={item.href}>
+              <img src={item.image} alt={item.label} />
+              <label>{item.label}</label>
+            </a>
+          ))
+        }
+      </div>
+    </div>
+  )
 }
