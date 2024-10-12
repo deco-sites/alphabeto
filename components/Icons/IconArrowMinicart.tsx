893bdb0fe3003c1a6c1cc7884a6ae53999e51dc3
Pedro Souza de Freitas <pedro.souza@econverse.com.br>
2024-10-12 09:58:34 -0300

    Publish 3 from localhost into origin/feature/28718/header

---
+++ components/Icons/IconArrowMinicart.tsx
@@ -0,0 +1,26 @@
+export function IconArrowMinicart() {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.16602 10.5H15.8327"
+        stroke="#D6DE23"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10 4.66699L15.8333 10.5003L10 16.3337"
+        stroke="#D6DE23"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
--- components/header/MinicartEmpty.tsx
+++ components/header/MinicartEmpty.tsx
@@ -1,4 +1,5 @@
 import { MinicartEmptyProps } from "../Session.tsx";
+import { IconArrowMinicart } from "../Icons/IconArrowMinicart.tsx"; 
 
 interface Props {
   content: MinicartEmptyProps | null;
@@ -7,20 +8,23 @@ interface Props {
 export function MinicartEmpty({ content }: Props) {
   console.log({ content });
   return (
-    <div>
-      <div>
+    <div class="relative h-full w-full flex justify-center">
+      <div class="pt-[130px] flex items-center flex-col gap-5">
         {content?.icon && <img src={content.icon} alt="Sacola" />}
-        <h4>{content?.title}</h4>    
+        <h4 class="font-['BeccaPerry'] text-[2rem] text-accent">{content?.title}</h4>    
       </div>
 
-      <div>
+      <div class="absolute bottom-0 w-[calc(100%-48px)] bg-secondary-content">
         <h4>{content?.itemsTitle}</h4>
 
         {
           content?.items.map(item => (
-            <a href={item.href}>
-              <img src={item.image} alt={item.label} />
-              <label>{item.label}</label>
+            <a href={item.href} class="flex justify-between">
+              <div>
+                <img src={item.image} alt={item.label} />
+                <label>{item.label}</label>
+              </div>
+              <IconArrowMinicart />
             </a>
           ))
         }
