2900a1c826e92cd2a55f3d91877f005487bd18fc
Guilherme da Silva Benevides <git@gui.dev.br>
2024-10-23 09:46:53 -0300

    feat: start results card

--- components/search/Searchbar/ProductCardSearch.tsx
+++ components/search/Searchbar/ProductCardSearch.tsx
@@ -6,8 +6,6 @@ import { formatPrice } from "../../../sdk/format.ts";
 import { relative } from "../../../sdk/url.ts";
 import { useOffer } from "../../../sdk/useOffer.ts";
 import { useSendEvent } from "../../../sdk/useSendEvent.ts";
-import AddToCartButton from "../../product/AddToCartButton.tsx";
-import WishlistButton from "../../wishlist/WishlistButton.tsx";
 
 interface Props {
   product: Product;
@@ -23,8 +21,8 @@ interface Props {
   class?: string;
 }
 
-const WIDTH = 287;
-const HEIGHT = 287;
+const WIDTH = 73;
+const HEIGHT = 102;
 const ASPECT_RATIO = `${WIDTH} / ${HEIGHT}`;
 
 function ProductCardSearch({ product, preload, itemListName, index, class: _class }: Props) {
@@ -33,11 +31,10 @@ function ProductCardSearch({ product, preload, itemListName, index, class: _clas
   const title = isVariantOf?.name ?? product.name;
   const [front, back] = images ?? [];
 
-  const { listPrice, price, seller = "1", availability } = useOffer(offers);
+  const { listPrice, price, availability } = useOffer(offers);
   const inStock = availability === "https://schema.org/InStock";
 
   const relativeUrl = relative(url);
-  const percent = listPrice && price ? Math.round(((listPrice - price) / listPrice) * 100) : 0;
 
   const item = mapProductToAnalyticsItem({ product, price, listPrice, index });
 
@@ -56,48 +53,28 @@ function ProductCardSearch({ product, preload, itemListName, index, class: _clas
   });
 
   return (
-    <div {...event} class={clx("card card-compact group text-sm", _class)}>
-      <figure class={clx("relative bg-base-200", "rounded border border-transparent", "group-hover:border-primary")} style={{ aspectRatio: ASPECT_RATIO }}>
+    <div {...event} class={clx("card card-side group text-sm gap-1", _class)}>
+      <figure class={clx("relative bg-base-200", "rounded border border-transparent", "group-hover:border-primary", "w-[73px] h-[102px]")} style={{ aspectRatio: ASPECT_RATIO }}>
         {/* Product Images */}
         <a href={relativeUrl} aria-label="view product" class={clx("absolute top-0 left-0", "grid grid-cols-1 grid-rows-1", "w-full", !inStock && "opacity-70")}>
           <Image src={front.url!} alt={front.alternateName} width={WIDTH} height={HEIGHT} style={{ aspectRatio: ASPECT_RATIO }} class={clx("object-cover", "rounded w-full", "col-span-full row-span-full")} sizes="(max-width: 640px) 50vw, 20vw" preload={preload} loading={preload ? "eager" : "lazy"} decoding="async" />
           <Image src={back?.url ?? front.url!} alt={back?.alternateName ?? front.alternateName} width={WIDTH} height={HEIGHT} style={{ aspectRatio: ASPECT_RATIO }} class={clx("object-cover", "rounded w-full", "col-span-full row-span-full", "transition-opacity opacity-0 lg:group-hover:opacity-100")} sizes="(max-width: 640px) 50vw, 20vw" loading="lazy" decoding="async" />
         </a>
-
-        {/* Wishlist button */}
-        <div class="absolute top-0 left-0 w-full flex items-center justify-between">
-          {/* Notify Me */}
-          <span class={clx("text-sm/4 font-normal text-black bg-error bg-opacity-15 text-center rounded-badge px-2 py-1", inStock && "opacity-0")}>Notify me</span>
-
-          {/* Discounts */}
-          <span class={clx("text-sm/4 font-normal text-black bg-primary bg-opacity-15 text-center rounded-badge px-2 py-1", (percent < 1 || !inStock) && "opacity-0")}>{percent} % off</span>
-        </div>
-
-        <div class="absolute bottom-0 right-0">
-          <WishlistButton item={item} variant="icon" />
-        </div>
       </figure>
 
-      <a href={relativeUrl} class="pt-5">
-        <span class="font-medium">{title}</span>
+      <a href={relativeUrl} className="">
+        <span class="font-bold text-accent text-xs leading-[18px]">{title}</span>
 
-        <div class="flex gap-2 pt-2">
-          {listPrice && <span class="line-through font-normal text-gray-400">{formatPrice(listPrice, offers?.priceCurrency)}</span>}
-          <span class="font-medium text-base-400">{formatPrice(price, offers?.priceCurrency)}</span>
+        <div class="flex items-center gap-[5px] pt-4">
+          {Boolean(listPrice) && (
+            <>
+              <span class="line-through text-xs leading-[14.4px] font-semibold text-[#c5c5c5]">{formatPrice(listPrice, offers?.priceCurrency)}</span>
+              <span class="text-sm leading-[14.4px] font-semibold text-primary">•</span>
+            </>
+          )}
+          <span class="text-sm leading-[16.8px] font-bold text-primary">{formatPrice(price, offers?.priceCurrency)}</span>
         </div>
       </a>
-
-      <div class="flex-grow" />
-
-      <div>
-        {inStock ? (
-          <AddToCartButton product={product} seller={seller} item={item} class={clx("btn", "btn-outline justify-start border-none !text-sm !font-medium px-0 no-animation w-full", "hover:!bg-transparent", "disabled:!bg-transparent disabled:!opacity-50", "btn-primary hover:!text-primary disabled:!text-primary")} />
-        ) : (
-          <a href={relativeUrl} class={clx("btn", "btn-outline justify-start border-none !text-sm !font-medium px-0 no-animation w-full", "hover:!bg-transparent", "disabled:!bg-transparent disabled:!opacity-75", "btn-error hover:!text-error disabled:!text-error")}>
-            Sold out
-          </a>
-        )}
-      </div>
     </div>
   );
 }
