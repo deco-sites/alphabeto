e8407c290d327793d025775d3753cc166c82f181
DeyvidJesus <deyvid.gondim@econverse.com.br>
2024-11-18 09:58:15 -0300

    feat: quickview modal

--- components/product/quickview/QuickViewModal.tsx
+++ components/product/quickview/QuickViewModal.tsx
@@ -1,41 +1,107 @@
 import { useScript } from "@deco/deco/hooks";
-import { ComponentChildren } from "preact";
-import { clx } from "site/sdk/clx.ts";
-import { useId } from "site/sdk/useId.ts";
-interface Props {
-  open?: boolean;
-  children?: ComponentChildren;
-  id?: string;
-  className?: string;
+
+const toggleDrawer = () => {
+  const drawer = document.getElementById("drawer-right-example");
+  if (drawer?.classList.contains("translate-x-full")) {
+    drawer?.classList.remove("translate-x-full");
+    drawer?.classList.add("translate-x-0");
+  } else {
+    drawer?.classList.remove("translate-x-0");
+    drawer?.classList.add("translate-x-full");
+  }
+
+  addEventListener("click", toggleDrawer)
 }
-const script = (id: string) => {
-  const handler = (e: KeyboardEvent) => {
-    if (e.key !== "Escape" && e.keyCode !== 27) {
-      return;
-    }
-    const input = document.getElementById(id) as HTMLInputElement | null;
-    if (!input) {
-      return;
-    }
-    input.checked = false;
-  };
-  addEventListener("keydown", handler);
-};
-function QuickViewModal({ children, open, id = useId(), className }: Props) {
+
+function QuickViewModal() {
   return (
     <>
-      <input id={id} checked={open} type="checkbox" class="modal-toggle" />
+      <div class="text-center">
+        <button
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          type="button"
+          hx-on="click: toggleDrawer()"
+        >
+          Show right drawer
+        </button>
+      </div>
       <div
-        class={clx("modal", className)}
+        id="drawer-right-example"
+        class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-80 dark:bg-gray-800"
+        aria-labelledby="drawer-right-label"
       >
-        {children}
-        <label class="modal-backdrop" for={id}>
-          Close
-        </label>
+        <h5
+          id="drawer-right-label"
+          class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"
+        >
+          Right drawer
+        </h5>
+        <button
+          type="button"
+          hx-on="click: toggleDrawer()"
+          class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <svg
+            class="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span class="sr-only">Close menu</span>
+        </button>
+        <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">
+          Supercharge your hiring by taking advantage of our{" "}
+          <a
+            href="#"
+            class="text-blue-600 underline font-medium dark:text-blue-500 hover:no-underline"
+          >
+            limited-time sale
+          </a>{" "}
+          for Flowbite Docs + Job Board. Unlimited access to over 190K
+          top-ranked candidates and the #1 design job board.
+        </p>
+        <div class="grid grid-cols-2 gap-4">
+          <a
+            href="#"
+            class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Learn more
+          </a>
+          <a
+            href="#"
+            class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Get access{" "}
+            <svg
+              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
       <script
         type="module"
-        dangerouslySetInnerHTML={{ __html: useScript(script, id) }}
+        dangerouslySetInnerHTML={{ __html: useScript(toggleDrawer) }}
       />
     </>
   );
