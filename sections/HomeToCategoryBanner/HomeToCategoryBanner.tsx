8fd8a06616071e0ee98a0a216e98e0c9a3e4b783
Guilherme da Silva Benevides <git@gui.dev.br>
2024-12-27 21:36:24 -0300

    fix: visual issues in buy with genre

--- sections/HomeToCategoryBanner/HomeToCategoryBanner.tsx
+++ sections/HomeToCategoryBanner/HomeToCategoryBanner.tsx
@@ -1,27 +1,26 @@
-import { ImageWidget } from "apps/admin/widgets.ts";
+import { Color, ImageWidget } from "apps/admin/widgets.ts";
 import { useDevice } from "@deco/deco/hooks";
-import Slider from "site/components/ui/Slider.tsx";
-import { LoadingFallbackProps } from "@deco/deco";
+import { Picture, Source } from "apps/website/components/Picture.tsx";
+import { clx } from "site/sdk/clx.ts";
+import Section from "site/components/ui/Section.tsx";
 
 interface HomeToCategoryBannerItem {
+  /**@title Label for CMS */
+  label: string;
   image: {
-    mobile?: {
-      src?: ImageWidget;
-      width?: number;
-      height?: number;
+    mobile: {
+      src: ImageWidget;
     };
 
     desktop: {
       src: ImageWidget;
-      width?: number;
-      height?: number;
     };
 
     alt: string;
   };
   categoryLink: string;
   buttonText: string;
-  buttonColor: string;
+  buttonColor: Color;
 }
 
 interface HomeToCategoryBannerProps {
@@ -32,56 +31,54 @@ interface HomeToCategoryBannerProps {
 export default function HomeToCategoryBanner(
   { title, items }: HomeToCategoryBannerProps,
 ) {
-  const device = useDevice();
-  const isDesktop = device === "desktop";
-
   return (
-    <div className={"desk:px-10 mt-[100px] mobile:px-4 w-full container"}>
-      <h2
-        className={"font-['BeccaPerry'] text-[40px] leading-[48px] font-medium text-[#676767] text-center mb-10"}
-      >
+    <div class="desk:mt-[100px] mt-[80px] w-full container">
+      <h2 class="font-beccaPerry desk:text-[40px] desk:leading-[48px] text-[28px] leading-[33px] font-medium text-[#676767] text-center mb-10">
         {title}
       </h2>
-      <Slider
-        className={"flex justify-between gap-4 max-w-full carousel carousel-center overflow-scroll"}
-      >
-        {items.map((item, index) => {
-          const image = isDesktop || !item.image.mobile?.src
-            ? item.image.desktop
-            : item.image.mobile;
-
+      <div class="grid grid-cols-1 desk:grid-cols-3 gap-4">
+        {items.map((item) => {
           return (
-            <Slider.Item
-              index={index}
-              className={"min-w-[335px] carousel-item"}
-            >
-              <div className={"relative"}>
-                <img
-                  className={"rounded-lg"}
-                  src={image.src}
-                  alt={item.image.alt}
-                  width={image.width}
-                  height={image.height}
-                />
-                <a
+            <article class="max-w-[443px] rounded-lg">
+              <a class="relative" href={item.categoryLink}>
+                <Picture>
+                  <Source
+                    src={item.image.desktop.src}
+                    width={443}
+                    height={496}
+                  />
+                  <Source
+                    src={item.image.mobile.src}
+                    width={443}
+                    height={496}
+                  />
+                  <img
+                    src={item.image.desktop.src}
+                    alt={item.image.alt}
+                    class="rounded-lg"
+                  />
+                </Picture>
+                <span
                   style={{ background: item.buttonColor }}
-                  className={"absolute top-[400px] w-full right-[20%] max-w-[243px] flex items-center justify-center rounded-full font-['BeccaPerry'] text-white desk:text-[40px] mobile:text-[28px] font-medium"}
-                  href={item.categoryLink}
+                  class={clx(
+                    "absolute desk:bottom-10 mobile:bottom-[30px] desk:h-[58px] desk:w-[242px] w-[165px] h-[44px] flex items-center justify-center rounded-full font-beccaPerry",
+                    "text-white left-1/2 -translate-x-1/2 desk:text-[40px] mobile:text-[28px] font-medium z-20",
+                  )}
                 >
                   {item.buttonText}
-                </a>
-              </div>
-            </Slider.Item>
+                </span>
+                <div class="absolute w-full h-auto left-0 bottom-0 aspect-[335/178] desk:aspect-[443/278] bg-gradient-to-b from-[#14141400] to-[#141414] rounded-b-lg" />
+              </a>
+            </article>
           );
         })}
-      </Slider>
+      </div>
     </div>
   );
 }
 
-export const LoadingFallback = (
-  props: LoadingFallbackProps<HomeToCategoryBannerProps>,
-) => (
-  // deno-lint-ignore no-explicit-any
-  <HomeToCategoryBanner {...(props as any)} loading="lazy" />
-);
+export const LoadingFallback = () => {
+  const device = useDevice();
+  const size = device === "desktop" ? 584 + 100 : 1044 + 80;
+  return <Section.Placeholder height={size + "px"} />;
+};
