39686678f1487adb10fd9f18ed18d98da2e3c1b0
Felipe Campos <felipe.fcampos7@gmail.com>
2025-01-23 16:43:24 -0300

    feat(faca sua boneca): code refactoring

--- islands/MiniMeDoll/MiniMe.tsx
+++ islands/MiniMeDoll/MiniMe.tsx
@@ -29,6 +29,7 @@ export default function MiniMe({ title, price, installments, image }: Props) {
   const [dollParts, setDollParts] = useState<string[]>([])
 
   const [IsSelected, setIsSelected] = useState(false)
+  const [selectedId, setSelectedId] = useState<string | null>(null)
 
   const [finalValue, setFinalValue] = useState('')
   const [count, setCount] = useState(0);
@@ -36,13 +37,24 @@ export default function MiniMe({ title, price, installments, image }: Props) {
   const fetchData = async () => {
     const data = await LoaderMiniMe();
     setData(data);
-    const types = await LoaderMiniMeTypes();
-    const typesSorted = [...types].sort((a, b) => a.ordem - b.ordem);
+    const typeSequence = await LoaderMiniMeTypes();
+    const typesSorted = [...typeSequence].sort((a, b) => a.ordem - b.ordem);
     setTypes(typesSorted);
   };
 
   const filterData = () => {
     let tempData: StateUpdater<CustomPart[]> = [];
+
+    console.log(types)
+
+    const typesMap = types.filter((item) => item.ordem)
+
+    console.log(typesMap[count])
+
+    const typesSequence = typesMap.filter((item) => item.ordem)
+
+    console.log("num: ", typesSequence)
+
     switch (count) {
       case 0:
         tempData = data.filter((item) => parseInt(item.id_tipo) === 5);
@@ -95,12 +107,18 @@ export default function MiniMe({ title, price, installments, image }: Props) {
   };
 
   const selectPart = (id: string) => {
-    setIsSelected(!IsSelected)
-    const selectedImage = filteredData.find((item) => item.id === id)
-    if(selectedImage){
-        setDollParts((prevParts) => [...prevParts, selectedImage.img_frente])
+    if(selectedId !== id){
+        setSelectedId(id)
+        setIsSelected(true)
+        const selectedImage = filteredData.find((item) => item.id === id)
+        if(selectedImage){
+            setDollParts((prevParts) => [...prevParts, selectedImage.img_frente])
+            }
+    } else {
+        setSelectedId(null)
+        setIsSelected(false)
+        }
     }
-  }
 
   useEffect(() => {
     fetchData();
@@ -125,7 +143,7 @@ export default function MiniMe({ title, price, installments, image }: Props) {
       <section class="container flex max-w-[1360px] w-full mb-[100px]">
         <section class="mr-[30px] px-[55px] py-[33px] bg-[#fff] border-dashed border-[1px] border-[#FF3800] rounded-[8px] max-w-[557px] h-[735px] w-full">
           <Image
-            src={dollParts.length > 0 ? dollParts[dollParts.length - 1] : image}
+            src={dollParts.length > 0 && IsSelected === true ? dollParts[dollParts.length - 1] : image}
             width={446}
             height={669}
           />
@@ -139,81 +157,18 @@ export default function MiniMe({ title, price, installments, image }: Props) {
           </div>
 
           <div class="flex items-center relative mb-[36px] w-full">
-            <p class="font-Quicksand bg-[#D6DE23] w-[40px] h-[40px] text-[20px] text-[#F98300] py-[4px] px-[14px] rounded-[50%] text-center font-bold">
-              1
-            </p>
-            <hr
-              class={count >= 0
-                ? `border-dashed border-b-[1px] w-[80px] border-[#F98300]`
-                : `border-dashed border-b-[1px] w-[80px] border-[#C5C5C5]`}
-            />
-            <p
-              class={count >= 1
+          {types.map((i, index) => (
+                <>
+                <p class={count >= index
                 ? `font-Quicksand bg-[#D6DE23] w-[40px] h-[40px] text-[20px] text-[#F98300] py-[4px] px-[14px] rounded-[50%] font-bold`
-                : `text-[#C5C5C5] bg-[#F5F4F1] font-Quicksand w-[40px] h-[40px] text-[20px] py-[4px] px-[14px] rounded-[50%] font-bold`}
-            >
-              2
-            </p>
-            <hr
-              class={count >= 1
+                : `text-[#C5C5C5] bg-[#F5F4F1] font-Quicksand w-[40px] h-[40px] text-[20px] py-[4px] px-[14px] rounded-[50%] font-bold`}>{i.ordem}</p>
+                {index !== types.length - 1 && (
+                <hr class={count >= index
                 ? `border-dashed border-b-[1px] w-[80px] border-[#F98300]`
-                : `border-dashed border-b-[1px] w-[80px] border-[#C5C5C5]`}
-            />
-            <p
-              class={count >= 2
-                ? `font-Quicksand bg-[#D6DE23] w-[40px] h-[40px] text-[20px] text-[#F98300] py-[4px] px-[14px] rounded-[50%] font-bold`
-                : `text-[#C5C5C5] bg-[#F5F4F1] font-Quicksand w-[40px] h-[40px] text-[20px] py-[4px] px-[14px] rounded-[50%] font-bold`}
-            >
-              3
-            </p>
-            <hr
-              class={count >= 2
-                ? `border-dashed border-b-[1px] w-[80px] border-[#F98300]`
-                : `border-dashed border-b-[1px] w-[80px] border-[#C5C5C5]`}
-            />
-            <p
-              class={count >= 3
-                ? `font-Quicksand bg-[#D6DE23] w-[40px] h-[40px] text-[20px] text-[#F98300] py-[4px] px-[14px] rounded-[50%] font-bold`
-                : `text-[#C5C5C5] bg-[#F5F4F1] font-Quicksand w-[40px] h-[40px] text-[20px] py-[4px] px-[14px] rounded-[50%] font-bold`}
-            >
-              4
-            </p>
-            <hr
-              class={count >= 3
-                ? `border-dashed border-b-[1px] w-[80px] border-[#F98300]`
-                : `border-dashed border-b-[1px] w-[80px] border-[#C5C5C5]`}
-            />
-            <p
-              class={count >= 4
-                ? `font-Quicksand bg-[#D6DE23] w-[40px] h-[40px] text-[20px] text-[#F98300] py-[4px] px-[14px] rounded-[50%] font-bold`
-                : `text-[#C5C5C5] bg-[#F5F4F1] font-Quicksand w-[40px] h-[40px] text-[20px] py-[4px] px-[14px] rounded-[50%] font-bold`}
-            >
-              5
-            </p>
-            <hr
-              class={count >= 4
-                ? `border-dashed border-b-[1px] w-[80px] border-[#F98300]`
-                : `border-dashed border-b-[1px] w-[80px] border-[#C5C5C5]`}
-            />
-            <p
-              class={count >= 5
-                ? `font-Quicksand bg-[#D6DE23] w-[40px] h-[40px] text-[20px] text-[#F98300] py-[4px] px-[14px] rounded-[50%] font-bold`
-                : `text-[#C5C5C5] bg-[#F5F4F1] font-Quicksand w-[40px] h-[40px] text-[20px] py-[4px] px-[14px] rounded-[50%] font-bold`}
-            >
-              6
-            </p>
-            <hr
-              class={count >= 5
-                ? `border-dashed border-b-[1px] w-[80px] border-[#F98300]`
-                : `border-dashed border-b-[1px] w-[80px] border-[#C5C5C5]`}
-            />
-            <p
-              class={count >= 6
-                ? `font-Quicksand bg-[#D6DE23] w-[40px] h-[40px] text-[20px] text-[#F98300] py-[4px] px-[14px] rounded-[50%] font-bold`
-                : `text-[#C5C5C5] bg-[#F5F4F1] font-Quicksand w-[40px] h-[40px] text-[20px] py-[4px] px-[14px] rounded-[50%] font-bold`}
-            >
-              7
-            </p>
+                : `border-dashed border-b-[1px] w-[80px] border-[#C5C5C5]`} />
+                )}
+                </>
+            ))}
           </div>
 
           <div class="mb-[36px]">
@@ -231,7 +186,7 @@ export default function MiniMe({ title, price, installments, image }: Props) {
               <div class="flex items-center">
                 {filteredData.map((item) => (
                   <>
-                    <div onClick={() => selectPart(item.id)} class="flex flex-col items-center w-[138px] h-[237] mr-[4px] bg-[#fff] rounded-[8px] border-[1px] border-[#D6DE23]">
+                    <div onClick={() => selectPart(item.id)} class={IsSelected && selectedId === item.id ? `flex flex-col items-center w-[138px] h-[237] mr-[4px] bg-[#fff] rounded-[8px] border-[1px] border-[#D6DE23]` : `flex flex-col items-center w-[138px] h-[237] mr-[4px] rounded-[8px]`}>
                       <img class="" src={item.img_frente} alt="" />
                       <p class="font-Quicksand text-[#7E7F88] text-[16px]">{item.nome}</p>
                     </div>
