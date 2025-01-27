b43b9e9f1a3db0694c225011841c9a997b7b20eb
Felipe Campos <felipe.fcampos7@gmail.com>
2025-01-27 16:10:17 -0300

    feat(faca sua boneca): storing all parts inside an array

--- islands/MiniMeDoll/MiniMe.tsx
+++ islands/MiniMeDoll/MiniMe.tsx
@@ -27,6 +27,7 @@ export default function MiniMe({ title, price, installments, image }: Props) {
   const [filteredData, setFilteredData] = useState<CustomPart[]>([]);
 
   const [dollParts, setDollParts] = useState<string[]>([])
+  const [dollPartsBehind, setDollPartsBehind] = useState<string[]>([])
 
   const [IsSelected, setIsSelected] = useState(false)
   const [selectedId, setSelectedId] = useState<string | null>(null)
@@ -71,18 +72,46 @@ export default function MiniMe({ title, price, installments, image }: Props) {
     }
   };
 
-  const selectPart = (id: string) => {
-    if(selectedId !== id){
-        setSelectedId(id)
-        setIsSelected(true)
+  const selectPart = (id: string, id_type: string) => {
+
+    const typesStorage: PartType[] = JSON.parse(localStorage.getItem('types') || '[]')
+
+    const typeOfIndex = typesStorage.reduce((map, type, index) => {
+        map[parseInt(type.id)] = index;
+        return map
+    }, {} as Record<number, number>)
+
+    const index = typeOfIndex[parseInt(id_type)]
+
+    if(dollParts[index] !== id){
+        if(selectedId === id){
+            const updateParts = [...dollParts];
+            const updateBehindParts = [...dollPartsBehind]
+            updateBehindParts[index] = ""
+            updateParts[index] = ""
+            setDollParts(updateParts)
+            setDollPartsBehind(updateBehindParts)
+            setSelectedId(null)
+            setIsSelected(false)
+        } else {
+        const updateParts = [...dollParts];
+        const updateBehindParts = [...dollPartsBehind]
         const selectedImage = filteredData.find((item) => item.id === id)
-        if(selectedImage){
-            setDollParts((prevParts) => [...prevParts, selectedImage.img_frente])
-            }
+        if(selectedImage) {
+            updateParts[index] = selectedImage.img_frente;
+            updateBehindParts[index] = selectedImage.img_costas;
+            setDollParts(updateParts)
+            setDollPartsBehind(updateBehindParts)
+            setSelectedId(id)
+            setIsSelected(true)
+            console.log("partes atualizadas: ", dollParts)
+            console.log("partes atualizadas(costas): ", dollPartsBehind)
+        }}
     } else {
-        setSelectedId(null)
-        setIsSelected(false)
-        }
+        console.log("O ID selecionado já está configurado.");
+    }
+
+    console.log(id, id_type)
     }
 
   useEffect(() => {
@@ -104,15 +133,36 @@ export default function MiniMe({ title, price, installments, image }: Props) {
 
   if (!data || !types) return null;
 
+  console.log('Doll parts: ', dollParts)
+  console.log('Doll parts(costas): ', dollPartsBehind)
+
   return (
     <>
       <section class="container flex max-w-[1360px] w-full mb-[100px]">
-        <section class="mr-[30px] px-[55px] py-[33px] bg-[#fff] border-dashed border-[1px] border-[#FF3800] rounded-[8px] max-w-[557px] h-[735px] w-full">
-          <Image
-            src={dollParts.length > 0 && IsSelected ? dollParts[dollParts.length - 1] : image}
+        <section class="relative mr-[30px] px-[55px] py-[33px] bg-[#fff] border-dashed border-[1px] border-[#FF3800] rounded-[8px] max-w-[557px] h-[835px] w-full">
+          
+          {dollParts.some((part) => part) ? (
+          dollParts.map((part, index) => part ? 
+          (
+            <Image
+            key={index}
+            src={part}
+            width={446}
+            height={669}
+            class={`absolute 
+                ${part === "https://alphabetoio.vtexassets.com/arquivos/boneco_frente_A01.png" ? '' 
+                : `z-[10]`} ${index > 4 ? 'hidden' : ''}`}
+          />
+          ) : null
+        )
+    )
+          : (
+            <Image
+            src={image}
             width={446}
             height={669}
           />
+          )}
         </section>
 
         <div class="max-w-[773px] w-full h-[735px]">
@@ -152,8 +202,8 @@ export default function MiniMe({ title, price, installments, image }: Props) {
               <div class="flex items-center">
                 {count !== types.length -1 && filteredData.map((item) => (
                   <>
-                    <div onClick={() => selectPart(item.id)} class={IsSelected && selectedId === item.id ? `flex flex-col items-center w-[138px] h-[237] mr-[4px] bg-[#fff] rounded-[8px] border-[1px] border-[#D6DE23]` : `flex flex-col items-center w-[138px] h-[237] mr-[4px] rounded-[8px]`}>
-                      <img class="" src={item.img_frente} alt="" />
+                    <div onClick={() => selectPart(item.id, item.id_tipo)} class={IsSelected && selectedId === item.id ? `flex flex-col items-center w-[138px] h-[237] mr-[4px] bg-[#fff] rounded-[8px] border-[1px] border-[#D6DE23]` : `flex flex-col items-center w-[138px] h-[237] mr-[4px] rounded-[8px] cursor-pointer hover:rounded-[8px] transition duration-300 hover:border-[0.5px] hover:border-[#D6DE23]`}>
+                      <img class="" src={item.img_frente} />
                       <p class="font-Quicksand text-[#7E7F88] text-[16px]">{item.nome}</p>
                     </div>
                   </>
